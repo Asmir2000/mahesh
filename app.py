b52769dfe6e47f22beb330b5d8fbb51f4523ca2e
Asmir2000 <asmirkhanusa@gmail.com>
2025-03-08 01:21:57 -0500

    adding 3 files to local repo

---
+++ app.py
@@ -0,0 +1,37 @@
+# Iterative approach
+def fibonacci_iterative(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+# Example usage
+print(list(fibonacci_iterative(10)))
+# Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+# Recursive approach
+def fibonacci_recursive(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+# Example usage
+for i in range(10):
+    print(fibonacci_recursive(i))
+# Output: 0 1 1 2 3 5 8 13 21 34
+
+# Recursive approach with memoization
+def fibonacci_recursive_memo(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    else:
+        memo[n] = fibonacci_recursive_memo(n-1, memo) + fibonacci_recursive_memo(n-2, memo)
+        return memo[n]
+
+# Example usage
+for i in range(10):
+    print(fibonacci_recursive_memo(i))
+# Output: 0 1 1 2 3 5 8 13 21 34
